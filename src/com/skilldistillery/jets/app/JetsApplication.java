d37cf6018b99972587bfa79fb455c2394e19ee9b
Shaun R <shaun.reass@icloud.com>
2021-05-28 16:23:28 -0500

    initial commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,15 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entity.AirField;
+
+public class JetsApplication {
+	
+	private AirField airField; // this is the only addition to this class.
+	
+	
+	public static void main(String[] args) {
+	
+
+	}
+
+}
